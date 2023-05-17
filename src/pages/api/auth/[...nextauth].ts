2dd3bcfddbf228efd4e63097dfea4d2ade12a38a
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-05-17 11:13:08 +0900

    feat: next auth api handler

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth"
+import type { NextAuthOptions } from "next-auth"
+
+import CredentialsProvider from "next-auth/providers/credentials"
+
+import { auth } from "../../../firebase/admin"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      credentials: {},
+      authorize: async ({ idToken }: any, _req) => {
+        if (idToken) {
+          try {
+            const decoded = await auth.verifyIdToken(idToken)
+
+            return { ...decoded }
+          } catch (e) {
+            console.error(e)
+          }
+        }
+        return null
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user }
+    },
+    async session({ session, token }) {
+      session.user.emailVerified = token.emailVerified
+      session.user.uid = token.uid
+      return session
+    }
+  }
+}
+
+export default NextAuth(authOptions)

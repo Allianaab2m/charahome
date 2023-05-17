4b41042e3e2d8c430c5e4c70f6c226b36a8ccb05
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-05-17 11:15:49 +0900

    feat: login checker on root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../pages/api/auth/[...nextauth]"
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  const user = session?.user
+
   return (
-    <div>Page</div>
+    <>
+      <div>Page</div>
+      <p>{JSON.stringify(user)}</p>
+    </>
   )
 }
